6ff2ebda1db149dd3d33b1ee5194d70dd7dfe669
sayordebbarma <sayor.debbarma2021@vitbhopal.ac.in>
2023-10-02 21:14:23 +0530

    Initial Commit

---
+++ LoopPlay/LoopPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopPlayApp.swift
+//  LoopPlay
+//
+//  Created by Sayor Debbarma on 02/10/23.
+//
+
+import SwiftUI
+
+@main
+struct LoopPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
